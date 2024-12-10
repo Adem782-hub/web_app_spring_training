b4424de706dd646746be7a47f601d71f5196d0b4
John Doe <johndoe@example.fr>
2024-12-10 21:27:53 +0100

    Partie 6 : Ajout de la persistance avec PostgreSQL

---
+++ src/main/java/fr/lernejo/todo/TodoEntity.java
@@ -0,0 +1,38 @@
+package fr.lernejo.todo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("todo")
+public class TodoEntity {
+    @Id
+    private Long id;
+    private String message;
+    private String author;
+
+    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
+
--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -1,22 +1,32 @@
 package fr.lernejo.todo;
 
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/todo")
 public class TodoListController {
-    private final List<Todo> todos = new ArrayList<>();
+    private final TodoRepository todoRepository;
+
+    public TodoListController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     @PostMapping
     public void addTodo(@RequestBody Todo todo) {
-        todos.add(todo);
+        TodoEntity entity = new TodoEntity();
+        entity.setMessage(todo.message());
+        entity.setAuthor(todo.author());
+        todoRepository.save(entity);
     }
 
     @GetMapping
     public List<Todo> getTodos() {
-        return todos;
+        return ((List<TodoEntity>) todoRepository.findAll())
+                .stream()
+                .map(entity -> new Todo(entity.getMessage(), entity.getAuthor()))
+                .collect(Collectors.toList());
     }
 }
 
